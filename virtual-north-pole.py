20445e739e128147cb610f3d05d3e07aab7b61e9
RozzershotZ <q13rtaylor@wgsf.net>
2020-12-07 19:50:27 +0000

    added a menu function

--- virtual-north-pole.py
+++ virtual-north-pole.py
@@ -1,11 +1,25 @@
 import time
 
+
+def Menu(options, acceptableMenuChoices):
+   theChoice = ""
+   while(theChoice not in acceptableMenuChoices):
+   
+      for option in options:
+         print(option)
+
+      theChoice = input("Choose ur thing idk: ").upper()
+   
+   return theChoice
+
 def GoodOrBad():
-   global Name 
+   global Name
    print("Hello " + Name + ", we are checking if you've been bad or nasty")
    time.sleep(1)
    print("Uh oh, you've been bad this decade")
 
 Name = input("Give name: ")
+
+menu = Menu(["1 - option1", "2 - option2", "3 - option3", "q - quit"], ["1", "2", "3","q"])
 
 GoodOrBad()
